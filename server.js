ff327b17291ebf35a6d6dfe94b00de6fe4ecadc6
Jansen Cruz <jansencruz12345@gmail.com>
2025-11-19 22:42:45 +0800

    Fix css bug again hopefully

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('.'));
+app.use(express.static('./'));
 
 // API endpoint for resources
 app.get('/api/resources', (req, res) => {
